7acefe0854eb25a32fa626d3f4e958803de91368
choiyunjeong <yoonj410@gmail.com>
2022-12-17 23:29:39 +0900

    ToDo App

---
+++ src/main/java/com/homework/unit2/RepositoryHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.homework.unit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homework/unit2/RepositoryHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.unit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
