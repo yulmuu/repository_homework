f430f777cf02787fc50daf0259e1ebf220f845d4
choiyunjeong <yoonj410@gmail.com>
2022-12-18 21:35:35 +0900

    최종이어라...

--- src/main/java/com/homework/unit2/Controller.java
+++ src/main/java/com/homework/unit2/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
     @GetMapping
+
     public String helloWorld(){
         return "To-do Application !";
     }
